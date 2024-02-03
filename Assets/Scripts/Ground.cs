ad72146b969e0430d0c9806be4ea193acf67c8b6
Moysed <nongalmondza12@gmail.com>
2024-02-03 17:59:35 +0700

    ground script not done yet

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ground : MonoBehaviour
+{
+    public Button button;
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

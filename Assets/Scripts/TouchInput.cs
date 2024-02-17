0fe7883d288fc00f9765a2443708b86d8a85762c
Parpaploy <116086218+Parpaploy@users.noreply.github.com>
2024-02-17 15:39:45 +0700

    .

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TouchInput : MonoBehaviour
 {
+
+    Camera _Camera;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,48 @@ public class TouchInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touches.Length > 0) 
+        { 
+            Touch myTouch = Input.GetTouch(0); 
+            if (myTouch.phase == TouchPhase.Began) 
+            { 
+                /*if (obj == null) 
+                { 
+                    obj = Instantiate (PrefabSprite); 
+                    obj.name = myTouch.fingerId.ToString(); 
+                    Vector3 newPosition = _Camera.ScreenToWorldPoint (myTouch.position); 
+                    newPosition.z = 0; obj.transform.position = newPosition; 
+                    SpriteRenderer spr = obj.GetComponent<SpriteRenderer>(); 
+                    if (spr != null) 
+                    { 
+                        spr.color = new Color (Random.Range (0, 100) / 100f, Random.Range (0, 100) / 100f, Random.Range (0, 100) / 100f);
+                    } 
+                } */
+            } 
+            
+            if (myTouch.phase == TouchPhase.Moved) 
+            { 
+                /*if (obj != null) 
+                { 
+                    if (obj.name.Contains(myTouch.fingerId.ToString()))
+                    { 
+                        Vector3 newPosition = _Camera.ScreenToWorldPoint (myTouch.position); 
+                        newPosition.z = 0; 
+                        obj.transform.position = newPosition; 
+                    } 
+                }*/
+            } 
+            if (myTouch.phase == TouchPhase.Ended || myTouch.phase == TouchPhase.Canceled) 
+            { 
+                /*if (obj != null)
+                { 
+                    if (obj.name.Contains(myTouch.fingerId.ToString())) 
+                    { 
+                        Destroy (obj.gameObject, 0.5f); 
+                        obj = null; 
+                    } 
+                }*/
+            } 
+        }
     }
 }
